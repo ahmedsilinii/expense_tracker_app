1066af2d811e53b47b958eb5ef4a0a4a25225e72
ahmedsilinii <ahmedsilini5@gmail.com>
2024-04-01 15:59:06 +0100

    added an Expense Model

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -10,6 +10,13 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Expenses');
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('Chart'),
+          Text('Expenses List'),
+        ],
+      ),
+    );
   }
 }
