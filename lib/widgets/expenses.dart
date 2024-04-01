7467691271c00194053b3dd1a8e108b38a84e5d4
ahmedsilinii <ahmedsilini5@gmail.com>
2024-04-01 23:20:08 +0100

    responsive main content

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -74,6 +74,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Add some!'),
     );
@@ -95,14 +97,26 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
-          )
-        ],
-      ),
+      body: width > 600
+          ? Row(
+              children: [
+                Container(
+                  width: 400,
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                )
+              ],
+            ),
     );
   }
 }
