0a03cadcdaba4c13a79f47214f296c41fdae1c48
ahmedsilinii <ahmedsilini5@gmail.com>
2024-04-01 18:34:23 +0100

    Update main.dart
    updating the main Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 51, 106, 215));
 void main() {
   runApp(const MainApp());
 }
@@ -10,8 +12,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Expenses(),
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+              kColorScheme.primaryContainer,
+            ),
+            foregroundColor: MaterialStateProperty.all<Color>(
+              kColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+              ),
+            ),
+      ),
+      home: const Expenses(),
     );
   }
 }
